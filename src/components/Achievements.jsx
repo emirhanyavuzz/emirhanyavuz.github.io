aaf52b6d6d39320acf2accd69e392041e70f6ae6
Emirhan Yavuz <emirhanyavuz666@gmail.com>
2025-12-02 23:16:58 +0300

    about text added

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -5,20 +5,34 @@ const Achievements = () => {
     const achievements = [
         {
             id: 1,
+            title: 'YKS-SAY',
+            score: '1180th',
+            description: 'University Entrance Exam',
+            icon: ''
+        },
+        {
+            id: 2,
+            title: 'YKS-SOZ',
+            score: '289th',
+            description: 'University Entrance Exam',
+            icon: ''
+        },
+        {
+            id: 3,
             title: 'TOEIC',
             score: '815',
             description: 'Foreign Language Exam (English)',
             icon: ''
         },
         {
-            id: 2,
+            id: 4,
             title: 'ALES-SAY',
             score: '94',
             description: 'Academic Personnel and Graduate Education Entrance Exam',
             icon: ''
         },
         {
-            id: 3,
+            id: 5,
             title: 'Bachelor\'s Degree',
             score: '3.02 GPA',
             description: 'Computer Engineering',
